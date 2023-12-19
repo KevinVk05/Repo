461f1c4bf9efc0ab74476cd2647c1eebacf42d69
MartaOrejasPelaez <mop500240@gmail.com>
2023-12-19 12:35:59 +0100

    Cicunferencia

--- src/Proyecto/triangulo.java
+++ src/Proyecto/triangulo.java
@@ -12,9 +12,20 @@ public class triangulo {
 		int altura=in.nextInt();
 		System.out.println("El area del triangulo es: "+areaTriangulo(base,altura));
 
-			}
+		System.out.println("Introduce el radio de la circunferencia");
+		double radio=in.nextDouble();
+		System.out.println("El area de la circunferencia es: "+areaCircun(radio));
+		System.out.println("El perimetro de la circunferencia es: "+perimetroCircun(radio));
+	}
+	
 	public static double areaTriangulo(int base, int altura) {
 		return (base*altura)/2;
+	}
+	public static double areaCircun(double radio) {
+		return 3.14*radio*radio;
+	}
+	public static double perimetroCircun(double radio) {
+		return 2*3.14*radio;
 	}
 
 	}
